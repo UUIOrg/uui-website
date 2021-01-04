6a3463cfd99b3319262ce10d81163ccdb316331c
Sayantan Samanta <sssamanta789@gmail.com>
2021-01-04 18:01:35 +0530

    Setup  with redux

---
+++ src/pages/form.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import SEO from '../components/seo'
+import styled from 'styled-components'
+
+const form = () => {
+    return (
+        <>
+            <SEO title="form" />
+            <ContactFormWrapper>
+                
+            </ContactFormWrapper>
+        </>
+    )
+}
+
+export default form
+
+
+const ContactFormWrapper = styled.section``
